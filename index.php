98a8c650cf320a5759faf8851678426dbcb8ee81
Florian Allione <florian.allione@ynov.com>
2020-08-09 12:42:11 +0200

    Page d'accueil + mise en .php pour import header et footer

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php require("header.html"); ?>
+<main>
+    <div class="index-left-col index-col">
+        <img src="img/maPhoto.png" alt="Photo de Florian" />
+        <p>
+            Je m'appelle ALLIONE Florian, j'ai 19 ans, actuellement en première année de Bachelor chez YNOV
+            Informatique
+            à Aix-en-Provence.<br>Je vous
+            présente mon
+            Blog Professionnel. Grand Passionné d'informatique, j'adore
+            apprendre sur ce domaine immensément grand.<br>Militaire de Réserve à côté, j'utilise un peu de mon
+            temps
+            libre pour
+            défendre les grandes valeurs de nos armées et de la République à la BA 125 d'ISTRES.<br>Je vous laisse
+            donc
+            naviguer sur ce Blog pour me découvir et en apprendre un peu plus sur moi.
+        </p>
+        <a href="#" title="Me Découvrir" class="index-button">ME DECOUVRIR</a>
+    </div>
+    <div class="index-right-col index-col">
+        <div class="index-r-col-title">
+            <h2>Contact</h2>
+            <hr class="under-title" />
+        </div>
+        <div class="index-r-col-contain">
+            <p>Mon E-mail : <a href="mailto:florian.allione@gmail.com"
+                    class="index-mail-button">florian.allione@gmail.com</a></p>
+            <a href="#" title="Me Contacter" class="index-button">ME CONTACTER</a>
+        </div>
+
+    </div>
+
+</main>
+<?php require("footer.html"); ?>
