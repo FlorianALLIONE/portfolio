9125528ba0444d0eb15c7b160284b475bcb45531
Florian Allione <florian.allione@ynov.com>
2020-08-24 22:12:37 +0200

    Ajout petite modif page stage

--- stage.php
+++ stage.php
@@ -9,8 +9,9 @@
                 <h2>DES AILES ET DES Z</h2>
                 <p>Aix-en-Provence | Mars 2020 - Avril 2020</p>
             </div>
+            <hr />
             <div class="stage-main">
-                <p>L'association "<strong>Des Ailes et Des Z</strong>" vient en aide aux enfants atteint de phobie scolaire. Cela concerne souvent des victimes d'harcelement qui ne veulent, par conséquent, plus retourner dans leur établissement.<br/><strong>Des Ailes et Des Z</strong> les aide en leur offrant un endroit où passer du temps avec d'autres enfants victime de phobie scolaire. Ils pourront faire des activités sportives, intellectuelles, pour leur redonner confiance en eux et leur permettre par la suite, de pouvoir retourner travailler à l'école. Le but est donc d'accompagner ses enfants pour ne pas les laisser seul chez eux à se morfondre, et ainsi les faire revenir dans un établissement scolaire.</p>
+                <p>L'association "<strong>Des Ailes et Des Z</strong>" vient en aide aux enfants atteint de phobie scolaire. Cela concerne souvent des victimes d'harcelement qui ne veulent, par conséquent, plus retourner dans leur établissement.<br/><strong>Des Ailes et Des Z</strong> les aide en leur offrant un endroit où passer du temps avec d'autres enfants victime de phobie scolaire. Ils pourront faire des activités sportives, intellectuelles, pour leur redonner confiance en eux et leur permettre par la suite, de pouvoir retourner travailler à l'école. Le but est donc d'accompagner ses enfants pour ne pas les laisser seul chez eux à se morfondre, et ainsi les faire revenir dans un système éducatif normal.</p>
             </div>
         </div>
     </div>
